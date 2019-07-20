283635e2c2a067862702983391ad36b20067d2d1
nehashinde <nehashinde472@gmail.com>
2019-07-20 12:20:41 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { PlayComponent } from './play/play.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SettingComponent } from './setting/setting.component';
+import { AddplayComponent } from './addplay/addplay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"play",component:PlayComponent},
+  { path:"gallary",component:GallaryComponent },
+  { path:"feedback",component:FeedbackComponent },
+  { path:"logout",component:LogoutComponent },
+  { path:"setting",component:SettingComponent },
+  { path:"addplay",component:AddplayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { PlayComponent } from './play/play.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SettingComponent } from './setting/setting.component';
+import { AddplayComponent } from './addplay/addplay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    HomeComponent,
+    SignupComponent,
+    PlayComponent,
+    GallaryComponent,
+    FeedbackComponent,
+    LogoutComponent,
+    SettingComponent,
+    AddplayComponent
   ],
   imports: [
     BrowserModule,
